b576f3c5a6f9e1802b71ceedc1248c58390800a9
Maria Skafida <mskafi28@gmail.com>
2018-03-08 09:57:39 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -95,9 +95,7 @@ function start() {
 		.style("fill", function(d) { return fill(d.party); })
 		.on("mouseover", mouseover)
 		.on("mouseout", mouseout);
-	
 		// google search the donor
-	
 		.on("click", function(d) { window.open("http://www.google.com/search?q=" + d.donor);});
 		// Alternative title based 'tooltips'
 		// node.append("title")
