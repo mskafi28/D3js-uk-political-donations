3befeaf51730a366f7d1983f93691fff4126531f
Maria Skafida <mskafi28@gmail.com>
2018-03-14 13:36:18 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -79,8 +79,6 @@ function transition(name) {
 	}
 
  	if (name === "group-by-amount"){
-		sound.currentTime=0; 
-		sound.play();
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
