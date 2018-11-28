9b3910d519393f80614e172a6a1422cb2b66ff23
Maria Skafida <mskafi28@gmail.com>
2018-11-28 17:05:06 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -49,7 +49,6 @@ function transition(name) {
 		$("#view-donor-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
-		$("#view-amount-type").fadeOut(250);
 		return total();
 		//location.reload();
 	}
@@ -59,7 +58,6 @@ function transition(name) {
 		$("#view-donor-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-party-type").fadeIn(1000);
-		$("#view-amount-type").fadeOut(250);
 		return partyGroup();
 	}
 	if (name === "group-by-donor-type") {
@@ -68,7 +66,6 @@ function transition(name) {
 		$("#view-party-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-donor-type").fadeIn(1000);
-		$("#view-amount-type").fadeOut(250);
 		return donorType();
 	}
 	if (name === "group-by-money-source")
@@ -77,7 +74,6 @@ function transition(name) {
 		$("#view-donor-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
 		$("#view-source-type").fadeIn(1000);
-		$("#view-amount-type").fadeOut(250);
 		return fundsType();
 	}
 	// gia ti nea katigoria Split by amount
@@ -89,7 +85,6 @@ function transition(name) {
 		$("#view-donor-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
-		$("#view-amount-type").fadeIn(1000);
 		return amountType();
 	}
 
@@ -161,16 +156,6 @@ function fundsType() {
 		.start();
 }
 
-// gia ti nea katigoria
-
-function amountType() {
-	
-	force.gravity(0)
-		.friction(0.85)
-		.charge(function(d) { return -Math.pow(d.radius, 2) / 2.5; })
-		.on("tick", amounts)
-		.start();
-}
 
 function parties(e) {
 	node.each(moveToParties(e.alpha));
@@ -194,12 +179,6 @@ function types(e) {
 			.attr("cy", function(d) {return d.y; });
 }
 
-function amounts(e) {
-	node.each(moveToAmount(e.alpha));
-
-		node.attr("cx", function(d) { return d.x; })
-			.attr("cy", function(d) {return d.y; });
-}
 
 function all(e) {
 	node.each(moveToCentre(e.alpha))
@@ -278,30 +257,6 @@ function moveToFunds(alpha) {
 	};
 }
 
-// gia tin katigoria Split by amount
-
-function moveToAmount(alpha) {
-	return function(d) {
-		var centreX;
-		var centreY;
-		if (d.value <= 500000){
-			centreX = svgCentre.x +70;
-			centreY = svgCentre.y -70;
-		} else if (d.value <= 5000000){
-			centreX = svgCentre.x +450;
-			centreY = svgCentre.y -70;
-		} else if (d.value <= 10000000){
-			centreX = svgCentre.x +70;
-			centreY = svgCentre.y +250;
-		} else {
-			centreX = svgCentre.x +500;
-			centreY = svgCentre.y +250;
-		}
-		
-		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 1.1;
-		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 1.1;
-	};
-}
 
 // Collision detection function by m bostock
 function collide(alpha) {
