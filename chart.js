7e2f4fa862aae5c9a76372ca64637bc8cfe9ee05
Maria Skafida <mskafi28@gmail.com>
2018-12-24 16:38:32 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -430,6 +430,6 @@ $(document).ready(function() {
       var id = d3.select(this).attr("id");
       return transition(id);
     });
-    return d3.csv("data/7500up.csv", display);
+    return d3.csv("stat.csv", display);
 
 });
